7e810d0edb878713de9fcc5c2566a2f8162bca69
DanielaDiaz <123435496+VenusDiaz@users.noreply.github.com>
2024-03-09 21:49:17 -0600

    Se agrego la clase ventanilla

---
+++ src/models/Ventanilla.h
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Ventanilla
+{
+private:
+    string bancoId;
+    int numeroDeVentanilla;
+    string personalVentanilla;
+    string supervisorId;
+};
